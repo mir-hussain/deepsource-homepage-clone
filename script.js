f14bdac3da6bf5f80d54e320fefbdaa172147312
mir-hussain <mirhussainmurtaza@gmail.com>
2022-06-09 03:04:57 +0600

    navbar animation added

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.from(".navbar-background", {
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".navbar-background",
+    markers: true,
+    start: "top top",
+    toggleActions: "restart pause reverse pause",
+    scrub: 1.5,
+  },
+});
+
 gsap.to(".hero-text", {
   opacity: 0,
   y: -50,
   duration: 1.5,
   scrollTrigger: {
     trigger: ".hero-text",
-    markers: true,
     start: "bottom 20%",
     end: "top top",
     toggleActions: "restart pause reverse pause",
